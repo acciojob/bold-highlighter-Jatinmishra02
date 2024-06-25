6d6affb551d5314a021463c2e47b67b27dbbedda
Jatin Mishra <104919571+Jatinmishra02@users.noreply.github.com>
2024-06-26 02:21:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
-    //Write your code here
-
-
+    // Get all <strong> elements
+    let boldElements = document.getElementsByTagName('strong');
+    // Change the color of each <strong> element to green
+    for (let i = 0; i < boldElements.length; i++) {
+        boldElements[i].style.color = 'green';
+    }
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+    // Get all <strong> elements
+    let boldElements = document.getElementsByTagName('strong');
+    // Revert the color of each <strong> element back to black
+    for (let i = 0; i < boldElements.length; i++) {
+        boldElements[i].style.color = 'black';
+    }
 }
